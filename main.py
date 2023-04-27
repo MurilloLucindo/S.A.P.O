3d66533dae1b30fa9025fdcbf00cd31c4c4f20ac
MurilloLucindo <murillo_lucindo_@outlook.com>
2023-04-27 16:41:34 -0300

    Pequena att do codigo main

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import pandas as pd
 import os
 
-tabela_ids = pd.read_excel('./Planilha_Teste_1.xlsx')
-# o grande gpt falou q divia fazer assim para os filtross
+tabela_ids = pd.read_excel("C:\Users\t_murillo.sanches\Desktop\Projeto Python\python-excel\Planilha_Teste_1.xlsx")
+# o grande gpt falou q devia fazer assim para os filtros
 filtro_path = "./Filtro.xlsx"
-filtro_files = os.listdir(filtro_path)
 
-codes = tabela_ids['UID,C,7'].tolist()
+filtro = "CODES"
+tabela_ids_coluna = "UID,C,7"
+
+#codes = tabela_ids['UID,C,7'].tolist()
 
 # remover duplicatas
-codes = set(codes)
-codes = list(codes)
+#codes = set(codes)
+#codes = list(codes)
 
 # para cada codigo
-for codes in filtro_files:
-    print (codes)
+for i in range(len(tabela_ids[tabela_ids_coluna])):
+    if tabela_ids[tabela_ids_coluna][i] == filtro_path[filtro][i]:
+        print("Os valores da linha", i+1, "são iguais nas duas planilhas.")
+    else:
+        print("Os valores da linha", i+1, "são diferentes nas duas planilhas.")
     # pra cada arquivo na pasta consulta
     # fazer uma lista de arquivos na pasta consulta, acho que da pra fazer com
     # a biblioteca 'os', pede pro gpt
